c31a92c2f03a8555ec9403826bfcb5adca5e1ce6
AndreaAvi <AndreaCA23@outlook.es>
2022-02-24 12:41:21 -0600

    Iniciamos el archivo Foco.java

---
+++ clases/ConceptosBasicos/src/ico/fes/Foco.java
@@ -0,0 +1,50 @@
+/*
+POO2209  Semestre 2022-2
+Clase 24.02.2022
+ */
+package ico.fes;
+
+/**
+ *
+ * @author Andi
+ */
+public class Foco {
+
+    private String color;
+    private String tipo;
+    private int tanano;
+
+    public Foco() {
+    }
+
+    public Foco(String color, String tipo, int tanano) {
+        this.color = color;
+        this.tipo = tipo;
+        this.tanano = tanano;
+    }
+
+    public int getTanano() {
+        return tanano;
+    }
+
+    public void setTanano(int tanano) {
+        this.tanano = tanano;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+}
